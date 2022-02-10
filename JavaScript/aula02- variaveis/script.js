7cc9f3e68784304c1aa1e9e33b604b476a4f11ab
Rodrigo <rodrigowilsonubj@gmail.com>
2022-02-10 02:00:11 -0300

    Inclusão de Javascript e Certificados

---
+++ JavaScript/aula02- variaveis/script.js
@@ -0,0 +1,53 @@
+/*/bollean
+var vOuF = false;
+console.log (typeof(vOuF));
+
+//number
+var numeroqualquer =1 ; 
+console.log (typeof(numeroqualquer));
+
+//string
+var nome = "rodrigo";
+console.log (typeof(nome));
+
+//escopo
+var escopoGlobal = "Global";
+console.log ( escopoGlobal);
+
+function escopolocal(){
+    let escopolocalinterno = "local";
+    console.log (escopolocalinterno)
+}
+escopolocal();
+ 
+//Atribuição
+var atribuição = "rodrigo";
+console.log (atribuição);
+
+// Comparação
+
+var comparação ="0" == 0;
+console.log (comparação);
+
+// ComparaçãoIdentica 
+ var comparaçãoIdentica = "0" === 0;
+ console.log (comparaçãoIdentica)
+
+ //maiorque
+ var maiorque = 5 > 2;
+ console.log (maiorque);
+
+ //menorque
+ var menorque = 2 <= 2;
+ console.log(menorque)*/
+
+
+ // e
+ var e = true && false ;
+ console.log (e);
+
+ //ou 
+var ou = true||false;
+console.log (ou);
+
+
